d6c209a0b0f816cc0d13d448fb179e3260db3446
Mim-Sky <horodyski.michal@gmail.com>
2024-11-15 23:13:16 +0000

    addeed types in mobile and desktop links

--- src/app/components/ui/desktopLink.tsx
+++ src/app/components/ui/desktopLink.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 
-export default function DesktopLink({ title, path }) {
+interface DesktopLinkProps {
+  title: string;
+  path: string;
+}
+
+export default function DesktopLink({ title, path }:DesktopLinkProps) {
   return (
     <Link href={path} className="p-4 ease-in-out duration-200 xl:text-xl font-semibold hover:text-earth-gray">{title}</Link>
   )
--- src/app/components/ui/mobileLink.tsx
+++ src/app/components/ui/mobileLink.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link"
 
-export default function MobileLink({ title, path }) {
+interface MobileLinkProps {
+  title: string;
+  path: string;
+}
+
+export default function MobileLink({ title, path }:MobileLinkProps) {
   return (
     <Link href={path} className="text-4xl hover:text-earth-gray">{title}</Link>  
   )

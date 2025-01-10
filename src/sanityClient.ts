9bd7e9f81a56990134e9857afaac34d98a262931
Mim-Sky <horodyski.michal@gmail.com>
2025-01-10 13:04:03 +0000

    changed any to Parameters in sanityCLient.ts. Typescript.

--- src/sanityClient.ts
+++ src/sanityClient.ts
@@ -1,15 +1,15 @@
-
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
 const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "o6fq908b",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",    
-  apiVersion: "2023-11-15",                            
-  useCdn: process.env.NODE_ENV === "production",                                    
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: "2023-11-15",
+  useCdn: process.env.NODE_ENV === "production",
 });
 
 const builder = imageUrlBuilder(client);
-export const urlFor = (source: any) => builder.image(source);
+
+export const urlFor = (source: Parameters<typeof builder.image>[0]) => builder.image(source);
 
 export default client;
